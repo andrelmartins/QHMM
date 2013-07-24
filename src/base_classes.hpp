ea47bfda8505550addc1b85c6dfaa21cd99a5639
Andre Martins <alm253@cornell.edu>
2013-07-23 22:11:31 -0400

    Interface (base classes) for transition and emission functions

---
+++ src/base_classes.hpp
@@ -0,0 +1,18 @@
+#ifndef BASE_CLASSES_HPP
+#define BASE_CLASSES_HPP
+
+#include "iter.hpp"
+
+class TransitionFunction {
+	public:
+		virtual double log_probability(Iter const & iter, int target) const = 0;
+    virtual ~TransitionFunction() {};
+};
+
+class EmissionFunction {
+  public:
+    virtual double log_probability(Iter const & iter) const = 0;
+    virtual ~EmissionFunction() {};
+};
+
+#endif
