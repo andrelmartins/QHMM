86860e539a18a49f2a48e31c9af2554e1e2ff7ab
Andre L. Martins <alm253@cornell.edu>
2013-08-05 10:22:58 -0400

    Discrete transition probabilities

---
+++ src/transitions/discrete.hpp
@@ -0,0 +1,81 @@
+#ifndef DISCRETE_HPP
+#define DISCRETE_HPP
+
+#include <cmath>
+#include <limits>
+#include "../base_classes.hpp"
+
+class Discrete : public TransitionFunction {
+public:
+  Discrete(int n_states, int n_targets, int * targets) : _n_states(n_states), _n_targets(n_targetes) {
+
+    _log_probs = new double[n_states];
+    _targets = new int[n_states];
+
+    for (int i = 0; i < n_targets; ++i)
+      _targets[i] = targets[i];
+
+    // default equi-probable
+    // set all to -Inf
+    for (int i = 0; i < _n_states; ++i)
+      _log_probs[i] = -std::numeric_limits<double>::infinity();
+
+    double log_prob = -log(_n_states);
+    for (int i = 0; i < _n_targets; ++i)
+      _log_probs[_targets[i]] = log_prob;
+  }
+
+  ~Discrete() {
+    delete[] _log_probs;
+    delete[] _targets;
+  }
+
+  virtual bool validParams(Params const & params) const {
+    double sum = 0.0;
+
+    for (int i = 0; i < params.length(); ++i)
+      sum = sum + params[i];
+
+    // TODO: maybe allow some tolerance ??
+    return params.length() == _n_states; && sum == 1.0;
+  }
+  
+  virtual void setParams(Params const & params) {
+    for (int i = 0; i < _n_targets; ++i)
+      _log_probs[_targets[i]] = log(params[i]);
+  }
+  
+  virtual double log_probability(int target) const {
+    return _log_probs[target];
+  }
+    
+  virtual double log_probability(Iter const & iter, int target) const {
+    return log_probability(target);
+  }
+
+private:
+  int _n_states;
+  int _n_targets;
+  int * _targets;
+  double * _log_probs;
+
+  void update_log_probs(double alpha) {
+    // set all to -Inf
+    for (int i = 0; i < _n_states; ++i)
+      _log_probs[i] = -std::numeric_limits<double>::infinity();
+    
+    // set actual probabilities
+    for (int i = 0; i < _n_targets; ++i)
+      _log_probs[_targets[i]] = log(
+      _log_probs[_targets[0]] = log(alpha); // self transition
+      
+      if (_n_targets > 1) {
+        double other = log(1 - alpha) - log(_n_targets - 1);
+        for (int i = 1; i < _n_targets; ++i)
+          _log_probs[_targets[i]] = other;
+      }
+
+
+};
+
+#endif
