b56461cb3ec82e539b07b98a2d043d8bb160fe9a
Andre Martins <alm253@cornell.edu>
2013-08-04 23:35:03 -0400

    Parameter class
    NOTE: I'll probably need to either expand this to support an "options" array or add another class for that.

---
+++ src/params.hpp
@@ -0,0 +1,65 @@
+#ifndef PARAMS_HPP
+#define PARAMS_HPP
+
+#include <stdexcept>
+#define INDEX_CHECK(index) if (index < 0 || index >= _length) throw std::out_of_range("index");
+
+class Params {
+
+public:
+  
+  Params(unsigned int length, const double * const values) : _length(length) {
+    _allFixed = false;
+    _fixed = new bool[length];
+    _values = new double[length];
+    for (int i = 0; i < length; ++i) {
+      _fixed[i] = false;
+      _values[i] = value[i];
+    }
+  }
+  
+  ~Params() {
+    delete _values;
+    delete _fixed;
+  }
+  
+  bool isAllFixed() {
+    return _allFixed;
+  }
+  
+  bool isFixed(unsigned int index) {
+    INDEX_CHECK(index);
+    return _fixed[index];
+  }
+  
+  void setFixed(unsigned int index, bool value) {
+    INDEX_CHECK(index);
+    _fixed[index] = value;
+    
+    if (_allFixed)
+      _allFixed = value;
+    else {
+      _allFixed = value;
+      for (int i = 0; i < _length && _allFixed; ++i)
+        _allFixed = _allFixed && _fixed[i];
+    }
+  }
+  
+  double operator[](unsigned int index) {
+    INDEX_CHECK(index);
+    return _values[index];
+  }
+  
+  int length() {
+    return _length;
+  }
+  
+private:
+  const unsigned int _length;
+  bool * _fixed;
+  bool _allFixed;
+  double * _values;
+  
+};
+
+#endif
