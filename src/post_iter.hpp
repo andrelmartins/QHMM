23303c3a6cb7c3ed11830ae68cbbb918a2c94665
Andre L. Martins <alm253@cornell.edu>
2013-08-28 15:13:37 -0400

    Transition Posterior Iterator (PostIter)
    This version will compute transition posteriors at every pass.
    Maybe later I'll make a version that pre-computes and stores the entire
    set. Storage requirements: (group size)x(n_targets)x(seq length).

---
+++ src/post_iter.hpp
@@ -0,0 +1,86 @@
+#ifndef POST_ITER_HPP
+#define POST_ITER_HPP
+
+#include "hmm.hpp"
+
+// Iterator for posterior transitions
+class PostIter {
+public:
+  PostIter(const double * forward, const double * backward, 
+	   const double * local_loglik,
+	   std::vector<TransitionFunction*> & group, HMM * hmm,
+	   Iter & iter) : _fw(forward), _bk(backward), 
+			  _local_logPx(local_loglik), _hmm(hmm),
+			  _group(group), _iter(iter) {
+    _group_size = group.size();
+    _group_ids = new int[_group_size];
+    for (unsigned int i = 0; i < _group_size; ++i)
+      _group_ids[i] = group[i]->stateID();
+
+    TransitionFunction * head = group[0];
+
+    _n_targets = head->n_targets(); /* all functions in a group must share
+				       the same number of targets */
+
+    _trans_post = new double[_n_targets * _group_size];
+    reset();
+  }
+
+  ~PostIter() {
+    delete[] _trans_post;
+    delete[] _group_ids;
+  }
+
+  void reset() {
+    _iter.resetFirst();
+    next(); /* will update values for first transition
+	       and cause iterator to be over second position
+	       as transitions take values at target
+	     */
+  }
+
+  double posterior(int grp_idx, int tgt_idx) {
+    return _trans_post[grp_idx * _n_targets + tgt_idx];
+  }
+
+  bool next() {
+    bool res = _iter.next();
+
+    if (res) {
+      double logPxi = _local_logPx[_iter.index()]; // NOTE: RHMM used local Px at src not target ...
+      _hmm->transition_posterior(_iter, _fw, _bk, logPxi, _group_size, _group_ids, _n_targets, _trans_post);
+
+    }
+    return res;
+  }
+
+  /* implement covariate interface if needed */
+  double covar(const int slot) const {
+    return _iter.covar(slot);
+  }
+    
+  double covar_i(const int slot, const int i) const {
+    return _iter.covar_i(slot, i);
+  }
+  
+  // offset with respect to current iterator sequence position
+  double covar_ext(const int slot, const int i, const int offset) const {
+    return _iter.covar_ext(slot, i, offset);
+  }
+
+  int length() const { return _iter.length(); }
+
+private:
+  const double * _fw;
+  const double * _bk;
+  const double * _local_logPx;
+  const HMM * _hmm;
+  const std::vector<TransitionFunction*> & _group;
+  unsigned int _group_size;
+  int * _group_ids;
+  int _n_targets;
+  Iter & _iter;
+  double * _trans_post;
+};
+
+#endif
