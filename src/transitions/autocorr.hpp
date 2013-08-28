f8a3526c274d2851759113d110c3fbf8cc53ece8
Andre L. Martins <alm253@cornell.edu>
2013-08-28 15:51:19 -0400

    EM Update for AutoCorr transitions

--- src/transitions/autocorr.hpp
+++ src/transitions/autocorr.hpp
@@ -40,9 +40,45 @@ class AutoCorr : public TransitionFunction {
       return _log_probs[target];
     }
     
-		virtual double log_probability(Iter const & iter, int target) const {
+    virtual double log_probability(Iter const & iter, int target) const {
       return log_probability(target);
     }
+
+  virtual void updateParams(EMSequences * sequences, std::vector<TransitionFunction*> * group) {
+    // sufficient statistics are the self and total expected counts
+    double expected_self_count = 0;
+    double expected_total_count = 0;
+
+    EMSequences::PosteriorTransitionIterators * siter = sequences->transition_iterators(*group);
+
+    // sum expected counts
+    do {
+      PostIter & piter = siter->iter();
+
+      piter.reset();
+      do {
+	for (unsigned int gidx = 0; gidx < group->size(); ++gidx) {
+	  expected_self_count += piter.posterior(gidx, 0);
+
+	  for (int tgt_idx = 1; tgt_idx < _n_targets; ++tgt_idx)
+	    expected_total_count += piter.posterior(gidx, tgt_idx);
+	}
+      } while (piter.next());
+    } while (siter->next());
+
+    // estimate parameters
+    double alpha = expected_self_count / expected_total_count;
+    
+    std::vector<TransitionFunction*>::iterator tf_it;
+    for (tf_it = group->begin(); tf_it != group->end(); ++tf_it) {
+      AutoCorr * tf = (AutoCorr*) *tf_it;
+
+      tf->update_log_probs(alpha);
+    }
+
+    delete siter;
+  }
+
     
   private:
     double * _log_probs;
@@ -90,7 +126,7 @@ class AutoCorrCovar : public TransitionFunction {
       throw std::logic_error("called homogeneous version of transition log_probability on non-homogeneous class");
     }
     
-		virtual double log_probability(Iter const & iter, int target) const {
+    virtual double log_probability(Iter const & iter, int target) const {
       if (_valid_states[target]) {
         double alpha = iter.covar(_covar_slot);
         if (target == _stateID)
