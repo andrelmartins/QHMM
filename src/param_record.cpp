fabca910ae97480dc6f579d0745cc6dff0c28e94
Andre L. Martins <alm253@cornell.edu>
2013-09-26 19:00:11 -0400

    Bug fix: incorrect assert range

--- src/param_record.cpp
+++ src/param_record.cpp
@@ -75,7 +75,7 @@ void ParamRecord::collect() {
 }
 
 double ParamRecord::value(int row, int position) const {
-  assert(row > 0 && row < this->size());
+  assert(row >= 0 && row < this->size());
   assert(position >= 0 && position < _size);
 
   return (*(_records[position]))[row];
