e9d66ca2b1159c8c6daa3fe8ddb748f7673d76f1
Andre Martins <alm253@cornell.edu>
2013-09-28 15:14:50 -0400

    Add exception try/catch to EM body

--- src/hmm_em.cpp
+++ src/hmm_em.cpp
@@ -73,59 +73,68 @@ EMResult HMM::em(std::vector<Iter*> & iters, double tolerance) {
   skip_transitions = sequences->unitarySequences();
 
   /* main EM loop */
-  prev_loglik = -std::numeric_limits<double>::infinity();
-  while (1) {
-    ++iter_count;
-
-    /* compute forward/backward per sequence => get log-lik */
-    cur_loglik = sequences->updateFwBk();
-
-    /* output cur_loglik & store current parameters */
-    update_records(result.param_trace);
-    printf("[%d] loglik: %g\n", iter_count, cur_loglik);
-
-    /* check log-lik */
-    if (cur_loglik < prev_loglik ||
-        cur_loglik - prev_loglik < tolerance)
-      break;
-
-    /* update parameters */
-
-    /* - transition functions */
-    if (!skip_transitions) {
-      std::vector<std::vector<TransitionFunction*> > tgroups = transition_groups();
-      std::vector<std::vector<TransitionFunction*> >::iterator tit;
-      for (tit = tgroups.begin(); tit != tgroups.end(); ++tit) {
-	std::vector<TransitionFunction*> group_i = *tit;
-	TransitionFunction * head = group_i[0];
-	
-	head->updateParams(sequences, &group_i);
-      }
-      refresh_transition_table(); // refresh internal caches
-    }
-
-    /* - emission functions */
-    std::vector<std::vector<EmissionFunction*> > groups = emission_groups();
-    std::vector<std::vector<EmissionFunction*> >::iterator it;
-    for (it = groups.begin(); it != groups.end(); ++it) {
-      std::vector<EmissionFunction*> group_i = *it;
-      EmissionFunction * head = group_i[0];
+  try {
+    prev_loglik = -std::numeric_limits<double>::infinity();
+    while (1) {
+      ++iter_count;
       
-      head->updateParams(sequences, &group_i);
+      /* compute forward/backward per sequence => get log-lik */
+      cur_loglik = sequences->updateFwBk();
+      
+      /* output cur_loglik & store current parameters */
+      update_records(result.param_trace);
+      printf("[%d] loglik: %g\n", iter_count, cur_loglik);
+      
+      /* check log-lik */
+      if (cur_loglik < prev_loglik ||
+          cur_loglik - prev_loglik < tolerance)
+        break;
+      
+      /* update parameters */
+      
+      /* - transition functions */
+      if (!skip_transitions) {
+        std::vector<std::vector<TransitionFunction*> > tgroups = transition_groups();
+        std::vector<std::vector<TransitionFunction*> >::iterator tit;
+        for (tit = tgroups.begin(); tit != tgroups.end(); ++tit) {
+          std::vector<TransitionFunction*> group_i = *tit;
+          TransitionFunction * head = group_i[0];
+          
+          head->updateParams(sequences, &group_i);
+        }
+        refresh_transition_table(); // refresh internal caches
+      }
+      
+      /* - emission functions */
+      std::vector<std::vector<EmissionFunction*> > groups = emission_groups();
+      std::vector<std::vector<EmissionFunction*> >::iterator it;
+      for (it = groups.begin(); it != groups.end(); ++it) {
+        std::vector<EmissionFunction*> group_i = *it;
+        EmissionFunction * head = group_i[0];
+        
+        head->updateParams(sequences, &group_i);
+      }
+      
+      /* */
+      prev_loglik = cur_loglik;
+      
+      /* check things went ok */
+      if (prev_loglik == -std::numeric_limits<double>::infinity()) {
+        printf("-Inf log likelihood! Aborted!\n");
+        break;
+      }
+      if (std::isnan(prev_loglik)) {
+        printf("NaN log likelihood! Aborted!\n");
+        break;
+      }
     }
-   
-    /* */
-    prev_loglik = cur_loglik;
+  } catch (QHMMException & e) {
+    // clean up memory
+    delete sequences;
+    delete_records(result.param_trace);
     
-    /* check things went ok */
-    if (prev_loglik == -std::numeric_limits<double>::infinity()) {
-      printf("-Inf log likelihood! Aborted!\n");
-      break;
-    }
-    if (std::isnan(prev_loglik)) {
-      printf("NaN log likelihood! Aborted!\n");
-      break;
-    }
+    e.stack.push_back("EM");
+    throw;
   }
 
   /* clean up */
